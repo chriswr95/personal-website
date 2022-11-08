b8ff93256f998875c5ef297b352114f94cb21d83
Chris Wright <chriswr@me.com>
2022-11-08 17:21:43 -0500

    added race times

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,12 +10,19 @@ function Home() {
           <p>
             <strong> $ whoami </strong>
             <br></br>
-            I'm a Bay Area based software engineer building (and breaking) things at Twitter.
+            I'm a San Francisco based software engineer building (and breaking) things at Twitter.
           </p>
           <p>
             In my free time I love getting outdoors. Mostly skiing, biking, and running.
             I'm currently reading the Foundation Series by Isaac Asimov in preperation for the AppleTV show.
             I also enjoying puttering around with crypto in my spare time.
+          </p>
+          <p>
+            I like to run and occasionally race official events. Here are the races I've run so far:
+            <ul>
+              <li> 2021 Portland Maraton - 4:06:36 </li>
+              <li> 2022 New York Marathon - 5:07:08 (it was hot) </li>
+            </ul>
           </p>
           <Tree />
         </div>
